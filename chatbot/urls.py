12e18e4e96103899f3dafa7c3037a783cf4a0b9b
E0ho <leeyeongho3404@gmail.com>
2024-08-02 00:30:50 +0900

    feat : 지원자, 수강생을 위한 chatbot 경로 설정

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,4 +3,5 @@ from chatbot import views
 
 urlpatterns = [
     path('applicant-bot/', views.applicant, name='applicant'),       # 지원자들을 위한 챗봇
+    path('student-bot/', views.student, name='student'),             # 수강생들을 위한 챗봇
 ]
